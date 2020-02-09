6c47abf40e7bb9069b474fc6da63fbc1263c70ae
Gaurav Parmar <gaurav.p.parmar@oracle.com>
2020-02-09 13:21:34 +0530

    prime factors

---
+++ src/main/java/PrimeFactors.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeFactors {
+    public static List<Integer> primeFactorsOf(int i) {
+        ArrayList<Integer> factors = new ArrayList<>();
+        for(int divisor = 2;divisor <= i;divisor++) {
+            for (; i % divisor == 0; i = i / divisor)
+                factors.add(divisor);
+        }
+        return factors;
+    }
+}
