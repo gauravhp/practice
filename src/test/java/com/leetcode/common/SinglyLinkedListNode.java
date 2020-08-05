2f2061e3417ed315ce6c7b17e36d0021a9c4e2b1
Gaurav Parmar <gaurav.p.parmar@oracle.com>
2020-08-05 14:25:25 +0530

    singly linked list

---
+++ src/test/java/com/leetcode/common/SinglyLinkedListNode.java
@@ -0,0 +1,10 @@
+package com.leetcode.common;
+
+public class SinglyLinkedListNode {
+    public SinglyLinkedListNode next;
+    public Integer value;
+
+    public SinglyLinkedListNode(Integer anInt) {
+        this.value = anInt;
+    }
+}
